ddb880d5fbfb19688945041f917bfb73dc3ec9c7
Dev Palo <nathannkweto@devpalo.tech>
2024-09-18 14:40:08 +0100

    Refactor __init__ to create tables
    	modified:   app/__init__.py
    
    Ensure that the application creates the neessary databases and tables
    nce initialised. You can do this by adding the create_all function to the
    appcontext of __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,21 @@
 
 from flask import Flask
 from .config import Config
-from .extensions import *
+from .extensions import db
 from .routes import api_blueprint
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    
+    # Initialize SQLAlchemy with the app
+    db.init_app(app)
 
     # Register blueprints (for routes)
     app.register_blueprint(api_blueprint)
 
     # Create tables before the first request
-    def create_tables():
+    with app.app_context():
         db.create_all()
 
     return app
